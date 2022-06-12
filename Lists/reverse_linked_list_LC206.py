e77096f4be06640e6134c9d137b0e3a6aad9dde2
loganschaufler <loganschaufler@git.com>
2022-06-12 11:53:21 -0400

    added algos

---
+++ Lists/reverse_linked_list_LC206.py
@@ -0,0 +1,24 @@
+"""Given the head of a singly linked list, reverse the list, and return the reversed list."""
+""" Input: head = [1,2,3,4,5]
+    Output: [5,4,3,2,1]"""
+
+#Time: O(n)
+#Space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        prev = None
+        
+        while(curr):
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        return prev
